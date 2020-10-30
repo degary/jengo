4e58307518945b6f58fed3e602a719291fbec4b3
邓辉 <denghui@blhcn.com>
2020-10-30 12:21:43 +0800

    first

---
+++ cmd/init.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"strings"
+)
+
+var desc = strings.Join([]string{
+	"该命令用于初始化项目时的其他选项,例如: ",
+	"-v 挂载选项等",
+}, "\n")
+var (
+	name  string
+	other string
+)
+
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "初始化项目",
+	Long:  "该子命令用于初始化项目",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("init")
+	},
+}
+
+func init() {
+	initCmd.Flags().StringVarP(&name, "name", "n", "", "项目名称")
+	initCmd.Flags().StringVarP(&other, "other", "o", "", desc)
+}
