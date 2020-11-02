fe3539432f0453b59bf4b1cb13e3c92120afb604
邓辉 <denghui@blhcn.com>
2020-11-02 18:12:53 +0800

    增加列出container

--- cmd/init.go
+++ cmd/init.go
@@ -2,7 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/degary/jengo/internal/dockeroper"
+	"github.com/degary/jengo/internal/initpro"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"path/filepath"
 	"strings"
 )
 
@@ -20,7 +24,29 @@ var initCmd = &cobra.Command{
 	Short: "初始化项目",
 	Long:  "该子命令用于初始化项目",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init")
+		fmt.Println("initproject")
+		viper.SetConfigName("config")
+		viper.SetConfigType("toml")
+		viper.AddConfigPath(".")
+		err := viper.ReadInConfig()
+		if err != nil {
+			fmt.Printf("read config failed:%v", err)
+		}
+		projectPath := viper.GetString("projectpath")
+		backupPath := viper.GetString("backuppath")
+		proPath := filepath.Join(projectPath, iniName)
+		backPath := filepath.Join(backupPath, iniName)
+		//创建项目目录
+		if err := initpro.Makedir(proPath); err != nil {
+			fmt.Errorf("创建项目目录报错了: %s\n", err)
+		}
+		//创建备份目录
+		if err := initpro.Makedir(backPath); err != nil {
+			fmt.Errorf("创建项目目录报错了: %s\n", err)
+		}
+		initpro.StartDocker()
+		containerList, err := dockeroper.ListContainer()
+		fmt.Println(containerList, err)
 	},
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/degary/jengo
 
 go 1.14
 
-require github.com/spf13/cobra v1.1.1
+require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/spf13/cobra v1.1.1
+	github.com/spf13/viper v1.7.0
+)
